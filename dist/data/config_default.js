8ee91b312274152a948b4de484c391007dd13f8f
iamfrank <frank@magenta.dk>
2019-10-15 16:14:21 +0200

    change config data

--- dist/data/config_default.js
+++ dist/data/config_default.js
@@ -4,24 +4,35 @@
 
 var GLOBAL_CONFIG = {
     base_url: './',
+    consumer: {
+        title: "Igaliku"
+    },
     units: [ 
         { 
+            title_da: "Diesel-generator 1",
+            title_kl: "Diesel-generator 1 på grønlandsk",
             link: "http://www.google.dk",
             graphic: "https://constructionreviewonline.com/wp-content/uploads/2017/03/cat-gen.jpg",
             layout: ["9.5rem","auto","auto","0"]
         },
         {
+            title_da: "Diesel-generator 2",
+            title_kl: "[Diesel-generator 2 på grønlandsk]",
             link: "http://www.google.dk",   
             graphic: "https://constructionreviewonline.com/wp-content/uploads/2017/03/cat-gen.jpg",
             layout: ["25rem","auto","auto","0"]
         },
         {
+            title_da: "Solceller",
+            title_kl: "[Solceller på grønlandsk]",
             link: "http://www.google.dk",
             graphic: "https://etap.com/images/default-source/product/photovoltaic-solar-panels/etap-photovoltaic-array-analysis-3.jpg?sfvrsn=6",
             layout: ["9.5rem","auto","auto","24rem"]
         }
     ],
     battery: {
+        title_da: "Batteribank",
+        title_kl: "[Batteribank på grønlandsk]",
         layout: ["27.75rem","auto","auto","24rem"]
     }
 }
